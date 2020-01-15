334c02b39296c890f7ecb4503e90da938ceca089
gipsy coder <ploissken@gmail.com>
2020-01-14 22:24:08 -0300

    first commit, first deploy. hooray

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const nodemon = require('gulp-nodemon')
+
+gulp.task('serve', function () {
+  var server = nodemon({
+    script: 'index.js',
+    ext: 'js'
+  })
+
+  server.on('restart', function () {
+    console.log('NodeJS restarted!')
+  }).on('crash', function () {
+    console.error('NodeJS has crashed!')
+    // Restart the server in 5 seconds
+    server.emit('restart', 5)
+  })
+})
